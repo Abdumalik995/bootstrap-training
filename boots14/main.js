52748bfe9150c4665acefc6155c5df5db6dbda25
Abdumalik995 <abdumalik.botirov.95@mail.ru>
2019-02-03 09:27:02 +0500

    vazifa buyichs boots14

--- boots14/main.js
+++ boots14/main.js
@@ -1,5 +1,7 @@
-/*$(document).ready(function() {
-	$('#jbtn').click(function() {
+$(document).ready(function() {
+	/*$('#jbtn').click(function() {
 		$('#inf1').css('display','block')
 	})
-})*/
+
+*/	$('[data-toggle="popover"]').popover();
+})
