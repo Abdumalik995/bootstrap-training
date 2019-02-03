09b168c411443a48d7d7a7057a8c84a58d8d8b8b
Abdumalik995 <abdumalik.botirov.95@mail.ru>
2019-02-03 10:58:37 +0500

    js effektlar qushdim

--- boots14/main.js
+++ boots14/main.js
@@ -1,7 +1,19 @@
 $(document).ready(function() {
-	/*$('#jbtn').click(function() {
-		$('#inf1').css('display','block')
-	})
+	$('#jbtn').click(function() {
+		$('.lastdiv').css('display','block')
+	});
+	/*$('#jbtn').dblclick(function() {
+		$('.lastdiv').css('display','none')
+	});*/
+	$('.lastdiv').mouseleave(function() {
+		$(this).css('display','none')
+	});
+	$('.jsimg').hover(function() {
+		$('.lastdiv1').css('display','block')
+	});
+	$('.lastdiv1').mouseleave(function() {
+		$(this).css('display','none')
+	});
 
-*/	$('[data-toggle="popover"]').popover();
+	$('[data-toggle="popover"]').popover();
 })
