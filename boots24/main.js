7c3509ee12cf3a26844910751728230fb70888fe
Abdumalik995 <abdumalik.botirov.95@mail.ru>
2019-03-11 18:19:21 +0500

    added new work pro

--- boots24/main.js
+++ boots24/main.js
@@ -1,3 +1,13 @@
+$(document).ready(function() {
+	$('[data-toggle="tooltip"]').tooltip();
 
+	 $('#no').click(function() {
+        $('#modal1').toggleClass("block");
+        console.log('boss');
+    });
+	 $(".close").click(function() {
+	 	$(this).parents("#modal1").css('display','none');
+	 })
+})
 
 	
